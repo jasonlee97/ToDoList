a1040192def0161cbf78f4f3eeab7bec24da5853
Arvin Rolos <arvin_rinaldo@yahoo.com>
2017-03-01 10:47:54 -0800

    Closes #13 - Added user roles

--- application/controllers/Views.php
+++ application/controllers/Views.php
@@ -19,7 +19,7 @@ class Views extends Application
 	 */
 	public function index()
     {
-        $this->data['pagetitle'] = 'Ordered TODO List - Group 11';
+        $this->data['pagetitle'] = 'Ordered TODO List - Group 12';
         $tasks = $this->tasks->all();   // get all the tasks
         $this->data['content'] = 'Ok'; // so we don't need pagebody
 
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -41,7 +41,7 @@ class Welcome extends Application
 
 		$this->data['display_tasks'] = $display_tasks;
 		$this->data['pagebody'] = 'homepage';
-		$this->data['pagetitle'] = 'To Do Manager - Group 11';
+		$this->data['pagetitle'] = 'To Do Manager - Group 12';
 		$this->render(); 
 	}
 
